6d0a8ad9477a5fa246e16e4657dc92288dd90f09
Nan_Ting <a3380991070@163.com>
2025-02-21 13:56:49 +0800

    配置系统

---
+++ Start/Main_.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace Start
+{
+    public class Main_
+    {
+        public static ClientWebSocket Socket { get; } = new ClientWebSocket();
+        public static CancellationToken CTokrn { get; } = new CancellationToken();
+        public static string Uri { get; set; } = string.Empty;
+        static void Main(string[] args)
+        {
+            string? useUri;
+            do
+            {
+                Console.WriteLine("输入Uri: \t");
+                useUri = Console.ReadLine();
+
+                try {
+                    new Uri(useUri);
+                } catch {
+                    useUri = null;
+                }
+
+            } while (useUri == null);
+            
+            建立连接(Socket, new Uri(useUri)).Wait();
+
+            Config.SetConf("SocketUri", useUri);
+            Console.WriteLine("Hello, World!");
+        }
+
+        private static async Task 建立连接(ClientWebSocket socket, Uri uri)
+        {
+            try {
+                await socket.ConnectAsync(uri, CTokrn);
+            } catch {
+                throw new Exception("建立连接: 请检查URI是否有效，服务器是否正常可访问");
+            }
+        }
+    }
+}
