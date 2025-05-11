cd51be7683b0cc3ec37d630d616fb6c985fc4d81
Nan_Ting <a3380991070@163.com>
2025-05-11 12:10:08 +0800

    更新维护的NetWorks

--- NapCatScript.Desktop/ViewModels/NetWorkModels/ListViewModel.cs
+++ NapCatScript.Desktop/ViewModels/NetWorkModels/ListViewModel.cs
@@ -11,16 +11,29 @@ using NapCatScript.Core.Services;
 using ReactiveUI;
 using BindingFlags = System.Reflection.BindingFlags;
 using ViewModelType = System.Type;
+using ServerType = System.Type;
+using System.Collections;
 
 namespace NapCatScript.Desktop.ViewModels.NetWorkModels;
 
 public class ListViewModel : ViewModelBase
 {
-    private List<ViewModelType> Types =
+    private enum ServerTypes
+    {
+        HttpServer,
+        HttpSseServer,
+        HttpClient,
+        WebSocketClient,
+        WebsocketServer
+    }
+
+    private static List<ViewModelType> s_viewModelTypes =
     [
         typeof(HttpServerViewModel),
     ];
-    
+
+    private static List<PropertyInfo> s_netWorksPropInfo = [];
+
     public ObservableCollection<object> NetWorkConfig { get; set; } = [];
 
     public ListViewModel()
@@ -29,16 +42,46 @@ public class ListViewModel : ViewModelBase
         SetConifg();
     }
 
-    public void ReceiveAddList(IInteractionContext<(object, Type), Unit> interaction)
+    static ListViewModel()
+    {
+        Type type = typeof(NetWorks);
+        var infos = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        s_netWorksPropInfo.AddRange(infos);
+    }
+
+    public void ReceiveAddList(IInteractionContext<(object, ServerType), Unit> interaction)
     {
         //Handel =>  ‰≥ˆ
         //RegionHandel =>  ‰»Î
-        (object obj, Type type) input = interaction.Input;
+        (object obj, ServerType type) input = interaction.Input;
         Add(input.obj, input.type);
         interaction.SetOutput(Unit.Default);
+
+        UpdateListWeb(input.obj, input.type);
+        //switch (input.type.Name) {
+        //    case nameof(ServerTypes.HttpClient):
+        //        break;
+        //    case nameof(ServerTypes.HttpServer):
+        //        break;
+        //    case nameof(ServerTypes.WebSocketClient):
+        //        break;
+        //    case nameof()
+        //}
     }
 
+    public void UpdateListWeb(object obj, ServerType type)
+    {
+        if (_netWorks is null) SetConifg();
+        if (_netWorks is null) return;
 
+        PropertyInfo? targetInfo = s_netWorksPropInfo.FirstOrDefault(f => f.Name.Contains(type.Name));
+        if (targetInfo is null) return;
+        IList? netWorkList = (IList?)targetInfo.GetValue(_netWorks);
+        if (netWorkList is null) return;
+        netWorkList.Add(obj);
+    }
+
+    private NetWorks? _netWorks;
     public async void SetConifg()
     {
         string s = await Core.MsgHandle.Utils.GetNetWorkConfig("6099", "napcat");
@@ -46,9 +89,13 @@ public class ListViewModel : ViewModelBase
             return;
         if(!netWorkJson.TryGetPropertyValue("network", out var network))
             return;
-
-        NetWorks config = network.Deserialize<NetWorks>()!;
-        Add(config);
+        try {
+            _netWorks = network.Deserialize<NetWorks>()!;
+        } catch (Exception e) {
+            Loging.Log.Erro("ListViewModel::SetConfig Õ¯¬Á≈‰÷√–Ú¡–ªØ ß∞‹!", e.Message, e.StackTrace);
+            return;
+        }
+        Add(_netWorks);
     }
 
     private void Add<T>(T obj) => AddAll([obj]);
@@ -61,7 +108,7 @@ public class ListViewModel : ViewModelBase
             if (typeName_ == null)
                 typeName = typeof(T).Name;
             else typeName = typeName_;
-            ViewModelType? viewType = Types.FirstOrDefault(f => f.Name.Contains(typeName));
+            ViewModelType? viewType = s_viewModelTypes.FirstOrDefault(f => f.Name.Contains(typeName));
             if (viewType is null)
                 return;
             ConstructorInfo ctorInfo = viewType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public, [])!;
